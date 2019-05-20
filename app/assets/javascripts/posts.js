ca82b366ae89d4fa287c237a18713fefba0e1442
AlexDNZ <paleozene@gmail.com>
2019-05-20 14:59:42 +0200

    add style

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -13,7 +13,11 @@ console.log("Welcome bro from posts.js!");
 
 $( document ).ready(function() {
   //$("#submit_btn").click(function() { document.getElementById("content-area").value = "" });
-
+  $("main-form").on("keypress", function (e) {
+    if (e.keyCode == 13) {
+        return false;
+    }
+      });
   getPosts;
 });
 
@@ -30,8 +34,13 @@ var getPosts = function(posts)
   {
     //$("post_div").html(element.content)
     var newDiv = document.createElement("div");
-    newDiv.innerHTML = element.user+ " dit : "+ element.content;
+    newDiv.innerHTML = element.user+ " dit : ";
+    var contentSpan = document.createElement("span");
+    contentSpan.innerHTML =  element.content;
+    contentSpan.classList.add("span-color")
+    newDiv.appendChild(contentSpan);
     document.getElementById("posts_div").appendChild(newDiv);
+    //document.getElementById("posts_div").appendChild(contentSpan);
     var objDiv = document.getElementById("posts_div");
     objDiv.scrollTop = objDiv.scrollHeight;
 
@@ -39,3 +48,20 @@ var getPosts = function(posts)
 
 }
 gon.watch( "posts" , {interval: 500}, getPosts );
+
+
+
+// document.getElementById("main-form").addEventListener("keydown", function(e) {
+//     if (!e) { var e = window.event; }
+//     e.preventDefault(); // sometimes useful
+//
+//     // Enter is pressed
+//     if (e.keyCode == 13) { submitFunction(); }
+// }, false);
+
+
+// document.getElementById('main-form').addEventListener("keypress", function(e) {
+//     if (e.keyCode == 13) {
+//         return false;
+//     }
+// });
