9a63892f98087feb2bc2ec9beb3c25348b497d55
AlexDNZ <paleozene@gmail.com>
2019-10-14 01:50:31 +0200

    Set onkeypress 13 fucntion for submit form Remove trash code disable logs

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,80 +1,61 @@
-
-console.log("Welcome bro from posts.js!");
+console.log("Welcome bro the Tchat Box !");
 var initialNumberPost = document.getElementsByClassName("single-post").length;
+var posts = gon.posts;
 
-$( document ).ready(function() {
+$(document).ready(function() {
   getPosts(posts)
-  //$("#submit_btn").click(function() { document.getElementById("content-area").value = "" });
-  // $("main-form").on("keypress", function (e) {
-  //   if (e.keyCode == 13) {
-  //       return false;
-  //   }
-  //     });
-      //scrollToBottom("posts_div")
-
 });
 
-
-
-function getPosts(posts)
-  {
-    document.getElementById("posts_div").innerHTML = "";
-    //scrollDown;
-    posts.forEach(function(element)
-    {
-      //$("post_div").html(element.content)
-      var newDiv = document.createElement("div");
-      newDiv.setAttribute("class","single-post")
-      newDiv.innerHTML = element.user+ " dit : ";
-      var contentSpan = document.createElement("span");
-      contentSpan.innerHTML =  element.content;
-      contentSpan.classList.add("span-color")
-      newDiv.appendChild(contentSpan);
-      document.getElementById("posts_div").appendChild(newDiv);
-      //document.getElementById("posts_div").appendChild(contentSpan);
-      scrollToBottom("posts_div")
-    })
-    initialNumberPost =  document.getElementsByClassName("single-post").length;
-  }
-
-gon.watch( "posts" , {interval: 500}, postCount );
-
-function postCount(posts)
-  {
-    newPostNumber = posts.length;
-    console.log("newPostNumber-->"+ newPostNumber);
-    console.log("initialNumberPost-->"+ initialNumberPost);
-    if (newPostNumber !== initialNumberPost )
-      {
-        getPosts(posts);
-      }
-  }
-
-
-function scrollToBottom(id)
-{
-   var div = document.getElementById(id);
-   div.scrollTop = div.scrollHeight - div.clientHeight;
-   console.log("hello from scrolling");
+function getPosts(posts) {
+  pushEnter();
+  document.getElementById("posts_div").innerHTML = "";
+  //scrollDown;
+  posts.forEach(function(element) {
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "single-post")
+    newDiv.innerHTML = element.user + " dit : ";
+    var contentSpan = document.createElement("span");
+    contentSpan.innerHTML = element.content;
+    contentSpan.classList.add("span-color")
+    newDiv.appendChild(contentSpan);
+    document.getElementById("posts_div").appendChild(newDiv);
+    //document.getElementById("posts_div").appendChild(contentSpan);
+    scrollToBottom("posts_div")
+  })
+  initialNumberPost = document.getElementsByClassName("single-post").length;
 }
 
-// var scrollDown = function(){
-  // var objDiv = document.getElementById("posts_div");
-  // objDiv.scrollTop = objDiv.scrollHeight;
-// }
+gon.watch("posts", {
+  interval: 500
+}, postCount);
 
+function postCount(posts) {
+  newPostNumber = posts.length;
+  // console.log("newPostNumber-->"+ newPostNumber);
+  // console.log("initialNumberPost-->"+ initialNumberPost);
+  if (newPostNumber !== initialNumberPost) {
+    getPosts(posts);
+  }
+}
 
-// document.getElementById("main-form").addEventListener("keydown", function(e) {
-//     if (!e) { var e = window.event; }
-//     e.preventDefault(); // sometimes useful
-//
-//     // Enter is pressed
-//     if (e.keyCode == 13) { submitFunction(); }
-// }, false);
+function scrollToBottom(id) {
+  var div = document.getElementById(id);
+  div.scrollTop = div.scrollHeight - div.clientHeight;
+  //console.log("hello from scrolling");
+}
 
+function pushEnter() {
+  var form = document.getElementById("main-form")
+  form.addEventListener("keydown", function(e) {
+    // if (!e) { var e = window.event; }
+    // e.preventDefault(); // sometimes useful
+    // Enter is pressed
+    if (e.keyCode == 13) {
+      submitFunction()
+    }
+  });
+}
 
-// document.getElementById('main-form').addEventListener("keypress", function(e) {
-//     if (e.keyCode == 13) {
-//         return false;
-//     }
-// });
+function submitFunction() {
+  document.getElementById("submit_btn").click();
+}
