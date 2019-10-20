d8e99fea6e1635123cffe73229f74f24d954db86
AlexDNZ <paleozene@gmail.com>
2019-10-20 14:16:31 +0200

    Set new root for session#new Add route for delete session buton Set ressources for sessions methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root 'sessions#new'
 
   get '/delete_all', to: 'posts#destroy_all'
   resources :posts, only: %i[index create destroy]
+  resources :sessions, only: %i[new index create ]
 end
