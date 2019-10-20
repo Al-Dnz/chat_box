602b1821dbb2465f8a6134e29779e93de754ce43
AlexDNZ <paleozene@gmail.com>
2019-10-20 02:19:26 +0200

    set session in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_134114) do
+ActiveRecord::Schema.define(version: 2019_10_19_195119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2019_05_19_134114) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.string "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
