622cee91b3ebb39b71a478bf32e28dd3627e7e31
AlexDNZ <paleozene@gmail.com>
2019-10-21 00:54:08 +0200

    Set session controller to include unique token in session creation Add flash notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,45 @@ class SessionsController < ApplicationController
   # end
 
   def index
+
   end
 
   def new
-    puts "$$$$$$-----|| HELOO FROM HOME PAGE ||----$$$$$$$$"
-    puts "$$$$$$-----|| Accumulatore==> #{@@accumulator} ||----$$$$$$$$"
-    @@accumulator += 1
+    Session.delete if session['username']
   end
 
   def create
-    session[:username] = params[:session][:username]
-    puts "******|| PARAMS_SESSION ==> #{session[:username]} ||*******"
-    session = Session.create(username: params[:session][:username])
-    redirect_to posts_path
+
+    username = params[:username]
+    @session = Session.new
+    if Session.exists?(username: username)
+      lastest_indicator = Session.where(username: username).count + 1
+      username = "#{username}##{lastest_indicator}"
+    end
+    @session.username = username
+    session[:username] = username
+    random_token = SecureRandom.hex(10)
+    until Session.find_by(token: random_token).nil?
+      random_token = SecureRandom.hex(10)
+    end
+    @session.token = random_token
+
+    if @session.save
+      flash[:notice] = "You signed up successfully"
+      flash[:color]= "valid"
+      puts "----------------------------------------------------------"
+      puts  "GOOOD"
+      puts "----------------------------------------------------------"
+      redirect_to posts_path
+    else
+      flash[:notice] = "Form is invalid"
+      flash[:color]= "invalid"
+      puts "----------------------------------------------------------"
+      puts "PAS BON !"
+      puts "----------------------------------------------------------"
+      redirect_to new_session_path
+    end
+
   end
 
   def delete
@@ -26,15 +52,19 @@ class SessionsController < ApplicationController
     session = Session.find_by(username: current_session_name)
     Session.delete(session) if session
     reset_session #if session[:username]
-    #ActionCable.server.broadcast 'presence_list', {presence: 0, username: current_session_name }
+    ActionCable.server.broadcast 'presence_list', {presence: false, username: current_session_name }
+    #ActionCable.server.remote_connections.where(current_user: current_session_name).disconnect
     redirect_to new_session_path
-    # ActionCable.server.remote_connections.where(current_user: session[:username]).disconnect
   end
 
   private
 
   def check_session
     redirect_to posts_path if session[:username]
+  end
+
+  def check_uniqueness(username)
+    Session.find_by(username: username).nil?
   end
 
 end
