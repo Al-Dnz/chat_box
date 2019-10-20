a8ec2d519589e6c81c9571984c987c2785bcf22a
AlexDNZ <paleozene@gmail.com>
2019-10-21 00:57:13 +0200

    remove useless code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,19 @@
 class PostsController < ApplicationController
-  #before_action :check_session
+  before_action :check_session
 
   def index
     @posts = Post.all.order(created_at: :asc)
-    gon.initial_number_post = @posts.count
-    gon.watch.posts = @posts
-    #gon.posts = @posts
-    @username = ""
-    @username = cookies[:user_name].to_s  if cookies[:user_name] != nil
+    @sessions = Session.all
+
+    if session[:username].nil? || Session.find_by(username: session[:username]).nil?
+      redirect_to new_session_path
+    else
+      ActionCable.server.broadcast 'presence_list', {presence: true, username: session[:username]}
+    end
 
   end
 
   def create
-    params[:user] == "" ? name  = "Anonyme" : name = params[:user]
-    if !params[:content].empty?
-      @post =  Post.create(user: name.capitalize, content: params[:content])
-      cookies[:user_name] = params[:user]
-      respond_to do |format|
-       format.js
-      end
-    end
-    #redirect_to root_path
   end
 
   def destroy
@@ -29,15 +22,15 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
-  def destroy_all
-    Post.all.each {|post| Post.delete(post)}
-    redirect_to root_path
-  end
+  # def destroy_all
+  #   Post.all.each {|post| Post.delete(post)}
+  #   redirect_to root_path
+  # end
 
   private
 
   def check_session
-    redirect_to new_session_path if session[:username] = nil
+    redirect_to new_session_path if !session
   end
 
 end
